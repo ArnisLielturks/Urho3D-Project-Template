d448452a80bfe4b0f00e0a491819e0fc8fa5111f
Arnis Lielturks <arnis.lielturks@gmail.com>
2018-12-10 18:46:32 +0200

    indent fixed

--- Source/Config/ConfigFile.h
+++ Source/Config/ConfigFile.h
@@ -34,60 +34,60 @@ typedef Urho3D::Vector<ConfigSection> ConfigMap;
 
 class ConfigFile : public Urho3D::Resource {
 public:
-  URHO3D_OBJECT(ConfigFile, Urho3D::Object);
+URHO3D_OBJECT(ConfigFile, Urho3D::Object);
 
 public:
-  ConfigFile(Urho3D::Context* context, bool caseSensitive = false);
-  ~ConfigFile() override = default;
+    ConfigFile(Urho3D::Context* context, bool caseSensitive = false);
+    ~ConfigFile() override = default;
 
-  static void RegisterObject(Urho3D::Context* context);
+    static void RegisterObject(Urho3D::Context* context);
 
-  void SetCaseSensitive(bool caseSensitive) {
-    caseSensitive_ = caseSensitive;
-  }
+    void SetCaseSensitive(bool caseSensitive) {
+        caseSensitive_ = caseSensitive;
+    }
 
-  /// Load resource from stream. May be called from a worker thread. Return true if successful.
-  virtual bool BeginLoad(Urho3D::Deserializer& source);
-  /// Save resource.
-  virtual bool Save(Urho3D::Serializer& dest) const;
-  /// Smart Save resource, replacing only the values, keeping whitespacing and comments.
-  virtual bool Save(Urho3D::Serializer& dest, bool smartSave) const;
+    /// Load resource from stream. May be called from a worker thread. Return true if successful.
+    virtual bool BeginLoad(Urho3D::Deserializer& source);
+    /// Save resource.
+    virtual bool Save(Urho3D::Serializer& dest) const;
+    /// Smart Save resource, replacing only the values, keeping whitespacing and comments.
+    virtual bool Save(Urho3D::Serializer& dest, bool smartSave) const;
 
-  /// Deserialize from a string. Return true if successful.
-  bool FromString(const Urho3D::String& source);
+    /// Deserialize from a string. Return true if successful.
+    bool FromString(const Urho3D::String& source);
 
-  const ConfigMap* GetMap() {
-    return &configMap_;
-  }
+    const ConfigMap* GetMap() {
+        return &configMap_;
+    }
 
-  bool Has(const Urho3D::String& section, const Urho3D::String& parameter);
+    bool Has(const Urho3D::String& section, const Urho3D::String& parameter);
 
-  const Urho3D::String GetString(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::String& defaultValue = Urho3D::String::EMPTY);
-  const int GetInt(const Urho3D::String& section, const Urho3D::String& parameter, const int defaultValue = 0);
-  const bool GetBool(const Urho3D::String& section, const Urho3D::String& parameter, const bool defaultValue = false);
-  const float GetFloat(const Urho3D::String& section, const Urho3D::String& parameter, const float defaultValue = 0.f);
-  const Urho3D::Vector2 GetVector2(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Vector2& defaultValue = Urho3D::Vector2::ZERO);
-  const Urho3D::Vector3 GetVector3(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Vector3& defaultValue = Urho3D::Vector3::ZERO);
-  const Urho3D::Vector4 GetVector4(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Vector4& defaultValue = Urho3D::Vector4::ZERO);
-  const Urho3D::Quaternion GetQuaternion(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Quaternion& defaultValue = Urho3D::Quaternion::IDENTITY);
-  const Urho3D::Color GetColor(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Color& defaultValue = Urho3D::Color::WHITE);
-  const Urho3D::IntRect GetIntRect(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::IntRect& defaultValue = Urho3D::IntRect::ZERO);
-  const Urho3D::IntVector2 GetIntVector2(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::IntVector2& defaultValue = Urho3D::IntVector2::ZERO);
-  const Urho3D::Matrix3 GetMatrix3(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Matrix3& defaultValue = Urho3D::Matrix3::IDENTITY);
-  const Urho3D::Matrix3x4 GetMatrix3x4(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Matrix3x4& defaultValue = Urho3D::Matrix3x4::IDENTITY);
-  const Urho3D::Matrix4 GetMatrix4(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Matrix4& defaultValue = Urho3D::Matrix4::IDENTITY);
+    const Urho3D::String GetString(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::String& defaultValue = Urho3D::String::EMPTY);
+    const int GetInt(const Urho3D::String& section, const Urho3D::String& parameter, const int defaultValue = 0);
+    const bool GetBool(const Urho3D::String& section, const Urho3D::String& parameter, const bool defaultValue = false);
+    const float GetFloat(const Urho3D::String& section, const Urho3D::String& parameter, const float defaultValue = 0.f);
+    const Urho3D::Vector2 GetVector2(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Vector2& defaultValue = Urho3D::Vector2::ZERO);
+    const Urho3D::Vector3 GetVector3(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Vector3& defaultValue = Urho3D::Vector3::ZERO);
+    const Urho3D::Vector4 GetVector4(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Vector4& defaultValue = Urho3D::Vector4::ZERO);
+    const Urho3D::Quaternion GetQuaternion(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Quaternion& defaultValue = Urho3D::Quaternion::IDENTITY);
+    const Urho3D::Color GetColor(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Color& defaultValue = Urho3D::Color::WHITE);
+    const Urho3D::IntRect GetIntRect(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::IntRect& defaultValue = Urho3D::IntRect::ZERO);
+    const Urho3D::IntVector2 GetIntVector2(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::IntVector2& defaultValue = Urho3D::IntVector2::ZERO);
+    const Urho3D::Matrix3 GetMatrix3(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Matrix3& defaultValue = Urho3D::Matrix3::IDENTITY);
+    const Urho3D::Matrix3x4 GetMatrix3x4(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Matrix3x4& defaultValue = Urho3D::Matrix3x4::IDENTITY);
+    const Urho3D::Matrix4 GetMatrix4(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Matrix4& defaultValue = Urho3D::Matrix4::IDENTITY);
 
-  void Set(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::String& value);
+    void Set(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::String& value);
 
-  // Returns header without bracket.
-  static const Urho3D::String ParseHeader(Urho3D::String line);
-  // Set property; Empty if no property.
-  static const void ParseProperty(Urho3D::String line, Urho3D::String& property, Urho3D::String& value);
-  // Strip comments and whitespace.
-  static const Urho3D::String ParseComments(Urho3D::String line);
+    // Returns header without bracket.
+    static const Urho3D::String ParseHeader(Urho3D::String line);
+    // Set property; Empty if no property.
+    static const void ParseProperty(Urho3D::String line, Urho3D::String& property, Urho3D::String& value);
+    // Strip comments and whitespace.
+    static const Urho3D::String ParseComments(Urho3D::String line);
 
 protected:
 
-  bool caseSensitive_;
-  ConfigMap configMap_;
+    bool caseSensitive_;
+    ConfigMap configMap_;
 };
--- Source/Config/ConfigManager.h
+++ Source/Config/ConfigManager.h
@@ -36,73 +36,73 @@ typedef Urho3D::HashMap<Urho3D::String, Urho3D::Variant> SettingsMap;
 class State;
 
 class ConfigManager : public Urho3D::Object {
-  URHO3D_OBJECT(ConfigManager, Urho3D::Object);
+URHO3D_OBJECT(ConfigManager, Urho3D::Object);
 
 public:
 
-  ConfigManager(Urho3D::Context* context, const Urho3D::String& defaultFileName = "Data/Config/config.cfg", bool caseSensitive = false, bool saveDefaultParameters = true);
-  ~ConfigManager() override = default;;
+    ConfigManager(Urho3D::Context* context, const Urho3D::String& defaultFileName = "Data/Config/config.cfg", bool caseSensitive = false, bool saveDefaultParameters = true);
+    ~ConfigManager() override = default;;
 
-  static void RegisterObject(Urho3D::Context* context);
+    static void RegisterObject(Urho3D::Context* context);
 
-  // Gets the settings map
-  SettingsMap& GetMap() {
-    return map_;
-  }
+    // Gets the settings map
+    SettingsMap& GetMap() {
+        return map_;
+    }
 
-  // Check if value exists
-  bool Has(const Urho3D::String& section, const Urho3D::String& parameter);
+    // Check if value exists
+    bool Has(const Urho3D::String& section, const Urho3D::String& parameter);
 
-  // Set value
-  void Set(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Variant& value);
+    // Set value
+    void Set(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Variant& value);
 
-  // Get value
-  const Urho3D::Variant Get(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Variant& defaultValue = Urho3D::Variant::EMPTY);
-  const Urho3D::String GetString(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::String& defaultValue = Urho3D::String::EMPTY);
-  const int GetInt(const Urho3D::String& section, const Urho3D::String& parameter, const int defaultValue = 0);
-  const int GetUInt(const Urho3D::String& section, const Urho3D::String& parameter, const unsigned defaultValue = 0);
-  const bool GetBool(const Urho3D::String& section, const Urho3D::String& parameter, const bool defaultValue = false);
-  const float GetFloat(const Urho3D::String& section, const Urho3D::String& parameter, const float defaultValue = 0.f);
-  const Urho3D::Vector2 GetVector2(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Vector2& defaultValue = Urho3D::Vector2::ZERO);
-  const Urho3D::Vector3 GetVector3(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Vector3& defaultValue = Urho3D::Vector3::ZERO);
-  const Urho3D::Vector4 GetVector4(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Vector4& defaultValue = Urho3D::Vector4::ZERO);
-  const Urho3D::Quaternion GetQuaternion(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Quaternion& defaultValue = Urho3D::Quaternion::IDENTITY);
-  const Urho3D::Color GetColor(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Color& defaultValue = Urho3D::Color::WHITE);
-  const Urho3D::IntRect GetIntRect(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::IntRect& defaultValue = Urho3D::IntRect::ZERO);
-  const Urho3D::IntVector2 GetIntVector2(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::IntVector2& defaultValue = Urho3D::IntVector2::ZERO);
-  const Urho3D::Matrix3 GetMatrix3(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Matrix3& defaultValue = Urho3D::Matrix3::IDENTITY);
-  const Urho3D::Matrix3x4 GetMatrix3x4(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Matrix3x4& defaultValue = Urho3D::Matrix3x4::IDENTITY);
-  const Urho3D::Matrix4 GetMatrix4(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Matrix4& defaultValue = Urho3D::Matrix4::IDENTITY);
+    // Get value
+    const Urho3D::Variant Get(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Variant& defaultValue = Urho3D::Variant::EMPTY);
+    const Urho3D::String GetString(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::String& defaultValue = Urho3D::String::EMPTY);
+    const int GetInt(const Urho3D::String& section, const Urho3D::String& parameter, const int defaultValue = 0);
+    const int GetUInt(const Urho3D::String& section, const Urho3D::String& parameter, const unsigned defaultValue = 0);
+    const bool GetBool(const Urho3D::String& section, const Urho3D::String& parameter, const bool defaultValue = false);
+    const float GetFloat(const Urho3D::String& section, const Urho3D::String& parameter, const float defaultValue = 0.f);
+    const Urho3D::Vector2 GetVector2(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Vector2& defaultValue = Urho3D::Vector2::ZERO);
+    const Urho3D::Vector3 GetVector3(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Vector3& defaultValue = Urho3D::Vector3::ZERO);
+    const Urho3D::Vector4 GetVector4(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Vector4& defaultValue = Urho3D::Vector4::ZERO);
+    const Urho3D::Quaternion GetQuaternion(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Quaternion& defaultValue = Urho3D::Quaternion::IDENTITY);
+    const Urho3D::Color GetColor(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Color& defaultValue = Urho3D::Color::WHITE);
+    const Urho3D::IntRect GetIntRect(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::IntRect& defaultValue = Urho3D::IntRect::ZERO);
+    const Urho3D::IntVector2 GetIntVector2(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::IntVector2& defaultValue = Urho3D::IntVector2::ZERO);
+    const Urho3D::Matrix3 GetMatrix3(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Matrix3& defaultValue = Urho3D::Matrix3::IDENTITY);
+    const Urho3D::Matrix3x4 GetMatrix3x4(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Matrix3x4& defaultValue = Urho3D::Matrix3x4::IDENTITY);
+    const Urho3D::Matrix4 GetMatrix4(const Urho3D::String& section, const Urho3D::String& parameter, const Urho3D::Matrix4& defaultValue = Urho3D::Matrix4::IDENTITY);
 
 
-  // Clears all settings
-  void Clear();
+    // Clears all settings
+    void Clear();
 
-  // Load settings from file
-  bool Load(bool overwriteExisting = true) {
-    return Load(defaultFileName_, overwriteExisting);
-  }
-  bool Load(const Urho3D::String& fileName, bool overwriteExisting = true);
-  bool Load(ConfigFile& configFile, bool overwriteExisting = true);
+    // Load settings from file
+    bool Load(bool overwriteExisting = true) {
+        return Load(defaultFileName_, overwriteExisting);
+    }
+    bool Load(const Urho3D::String& fileName, bool overwriteExisting = true);
+    bool Load(ConfigFile& configFile, bool overwriteExisting = true);
 
-  // Save settings to file
-  bool Save(bool smartSave = true) {
-    return Save(defaultFileName_, smartSave);
-  }
-  bool Save(const Urho3D::String& fileName, bool smartSave = true);
-  bool Save(ConfigFile& configFile);
-  void SaveSettingsMap(Urho3D::String section, SettingsMap& map, ConfigFile& configFile);
-
-protected:
-
-  SettingsMap* GetSection(const Urho3D::String& section, bool create = false);
+    // Save settings to file
+    bool Save(bool smartSave = true) {
+        return Save(defaultFileName_, smartSave);
+    }
+    bool Save(const Urho3D::String& fileName, bool smartSave = true);
+    bool Save(ConfigFile& configFile);
+    void SaveSettingsMap(Urho3D::String section, SettingsMap& map, ConfigFile& configFile);
 
 protected:
 
-  bool saveDefaultParameters_;
-  bool caseSensitive_;
+    SettingsMap* GetSection(const Urho3D::String& section, bool create = false);
 
-  Urho3D::String defaultFileName_;
+protected:
 
-  SettingsMap map_;
+    bool saveDefaultParameters_;
+    bool caseSensitive_;
+
+    Urho3D::String defaultFileName_;
+
+    SettingsMap map_;
 };

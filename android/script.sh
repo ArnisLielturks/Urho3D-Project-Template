b33d8c3ec3b1bdb0d2ee192aeeff5ffd23b18c5a
Arnis Lielturks <arnis.lielturks@gmail.com>
2019-03-06 13:56:11 +0200

    script update

--- android/script.sh
+++ android/script.sh
@@ -2,7 +2,7 @@
 rm -rf Urho3D
 git clone https://github.com/ArnisLielturks/Urho3D.git --depth 1
 mkdir Urho3D/Source/EmptyProject
-cp -rf Source/* Urho3D/Source/EmptyProject/*
+cp -rf Source/* Urho3D/Source/EmptyProject/
 cp android/CMakeLists.txt Urho3D/Source/EmptyProject/
 echo "add_subdirectory (\${CMAKE_CURRENT_SOURCE_DIR}/../../Source/EmptyProject EmptyProject)" >> Urho3D/android/launcher-app/CMakeLists.txt
 cd Urho3D

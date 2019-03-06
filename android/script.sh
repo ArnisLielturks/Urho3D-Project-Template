b64c7f82a86e9df1354e884d98a0554be838df88
Arnis Lielturks <arnis.lielturks@gmail.com>
2019-03-06 15:11:44 +0200

    asset dir added

--- android/script.sh
+++ android/script.sh
@@ -2,6 +2,12 @@
 rm -rf Urho3D
 git clone https://github.com/ArnisLielturks/Urho3D.git --depth 1
 mkdir Urho3D/Source/EmptyProject
+
+rm -rf Urho3D/bin/Data
+rm -rf Urho3D/bin/CoreData
+cp -r bin/Data Urho3D/bin/Data
+cp -r bin/CoreData Urho3D/bin/CoreData
+
 cp -rf Source/* Urho3D/Source/EmptyProject/
 cp android/CMakeLists.txt Urho3D/Source/EmptyProject/
 echo "add_subdirectory (\${CMAKE_CURRENT_SOURCE_DIR}/../../Source/EmptyProject EmptyProject)" >> Urho3D/android/launcher-app/CMakeLists.txt

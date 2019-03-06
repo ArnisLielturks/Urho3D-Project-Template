9ee5009c9627df623cf95187b097e70547df66fa
Arnis Lielturks <arnis.lielturks@gmail.com>
2019-03-06 14:12:46 +0200

    android build scripts updated

--- android/script.sh
+++ android/script.sh
@@ -6,7 +6,7 @@ cp -rf Source/* Urho3D/Source/EmptyProject/
 cp android/CMakeLists.txt Urho3D/Source/EmptyProject/
 echo "add_subdirectory (\${CMAKE_CURRENT_SOURCE_DIR}/../../Source/EmptyProject EmptyProject)" >> Urho3D/android/launcher-app/CMakeLists.txt
 cd Urho3D
-./script/dockerized.sh android ./gradlew --stacktrace -P URHO3D_LIB_TYPE=SHARED -P URHO3D_TOOLS=0 -P ANDROID_ABI=armeabi-v7a -P URHO3D_SAMPLES=0 build
+./script/dockerized.sh android ./gradlew --stacktrace -P URHO3D_LIB_TYPE=SHARED -P URHO3D_TOOLS=0 -P ANDROID_ABI=armeabi-v7a -P URHO3D_SAMPLES=0 -P CMAKE_BUILD_TYPE=Debug assembleDebug
 
 curl -X POST \
         -H "Content-Type: multipart/form-data" \

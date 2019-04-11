0b2b93bc9cdd19979675ea1c2791295709f5630a
Arnis Lielturks <arnis.lielturks@gmail.com>
2019-04-11 16:31:12 +0300

    android config files cleaned

--- android/script.sh
+++ android/script.sh
@@ -1,25 +1,41 @@
 #!/bin/bash
+# For debugging remove the Urho3D folder every time we run the script
 #rm -rf Urho3D
-git clone https://github.com/Urho3D/Urho3D.git
-mkdir Urho3D/Source/ProjectTemplate
 
+# Clone the latest engine version
+git clone https://github.com/Urho3D/Urho3D.git
+
+# Remove original Urho3D asset directories
 rm -rf Urho3D/bin/Data
 rm -rf Urho3D/bin/CoreData
+
+# Copy our own asset directories
 cp -r bin/Data Urho3D/bin/Data
 cp -r bin/CoreData Urho3D/bin/CoreData
 
+# Create our project subdirectory
+mkdir Urho3D/Source/ProjectTemplate
+
+# Copy our sample to the Urho3D subdirectory to build it the same ways as the samples are built
 cp -rf Source/* Urho3D/Source/ProjectTemplate/
+
+# Add our custom CMakeLists to allow to build our subproject
 cp android/CMakeLists.txt Urho3D/Source/ProjectTemplate/
 
+# Remove original Urho3D android application files
 rm -rf Urho3D/android/urho3d-lib
 rm -rf Urho3D/android/launcher-app
+
+# Copy our versions of the apps
 cp -r android/urho3d-lib Urho3D/android/urho3d-lib
 cp -r android/launcher-app Urho3D/android/launcher-app
 
-#echo "add_subdirectory (\${CMAKE_CURRENT_SOURCE_DIR}/../../Source/ProjectTemplate ProjectTemplate)" >> Urho3D/android/launcher-app/CMakeLists.txt
 cd Urho3D
+
+# Run dockerized android build to genereate libProjectTemplate.so file
 ./script/dockerized.sh android ./gradlew --stacktrace -P URHO3D_LIB_TYPE=SHARED -P URHO3D_TOOLS=0 -P ANDROID_ABI=armeabi-v7a -P URHO3D_SAMPLES=0 -P CMAKE_BUILD_TYPE=Debug assembleDebug
 
+# Upload the application somewhere we need
 curl -X POST \
         -H "Content-Type: multipart/form-data" \
         -F file=@android/launcher-app/build/outputs/apk/debug/launcher-app-armeabi-v7a-debug.apk \

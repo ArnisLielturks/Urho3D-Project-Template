dcbaf62025ad287b920465e8f67c548ae8c3fa70
Arnis Lielturks <arnis.lielturks@gmail.com>
2019-10-11 12:49:36 +0300

    Update script.sh

--- android/script.sh
+++ android/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # For debugging remove the Urho3D folder every time we run the script
-#rm -rf Urho3D
+rm -rf Urho3D
 
 # Clone the latest engine version
 git clone https://github.com/Urho3D/Urho3D.git
@@ -33,6 +33,6 @@ cp -r android/launcher-app Urho3D/android/launcher-app
 cd Urho3D
 
 # Run dockerized android build to genereate libProjectTemplate.so file
-./script/dockerized.sh android ./gradlew --stacktrace -P URHO3D_LIB_TYPE=SHARED -P URHO3D_TOOLS=0 -P ANDROID_ABI=armeabi-v7a -P URHO3D_SAMPLES=0 -P CMAKE_BUILD_TYPE=Release assembleDebug
+./script/dockerized.sh android ./gradlew -P URHO3D_LIB_TYPE=SHARED -P URHO3D_TOOLS=0 -P ANDROID_ABI=armeabi-v7a -P URHO3D_SAMPLES=0 -P CMAKE_BUILD_TYPE=Release assembleDebug
 
 cd ..

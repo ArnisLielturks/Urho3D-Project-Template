416b1658f10162a492705c7cbd85179f22a83807
Arnis Lielturks <arnis.lielturks@gmail.com>
2020-03-06 00:02:40 +0200

    web build script updates

--- web/script.sh
+++ web/script.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# For debugging remove the Urho3D folder every time we run the script
-#rm -rf Urho3D
 
 # Clone the latest engine version
 git clone https://github.com/Urho3D/Urho3D.git --depth=1 Urho3D-web
@@ -8,13 +6,6 @@ git clone https://github.com/Urho3D/Urho3D.git --depth=1 Urho3D-web
 # Remove original Urho3D asset directories
 rm -rf Urho3D-web/bin/Data
 rm -rf Urho3D-web/bin/CoreData
-
-rm -rf Urho3D-web/Source/Samples/0*
-rm -rf Urho3D-web/Source/Samples/1*
-rm -rf Urho3D-web/Source/Samples/2*
-rm -rf Urho3D-web/Source/Samples/3*
-rm -rf Urho3D-web/Source/Samples/4*
-rm -rf Urho3D-web/Source/Samples/5*
 
 # Copy our own asset directories
 cp -r bin/Data Urho3D-web/bin/Data
@@ -27,11 +18,15 @@ mkdir Urho3D-web/Source/ProjectTemplate
 cp -rf Source/* Urho3D-web/Source/ProjectTemplate/
 cp -rf web/application.html Urho3D-web/Source/ProjectTemplate/application.html
 cp -rf web/CMakeLists.txt Urho3D-web/Source/ProjectTemplate/CMakeLists.txt
+
+# Use our custom dockerized script which is supported by Github actions
 cp -rf script/dockerized.sh Urho3D-web/script/dockerized.sh
+
+# Use custom CMake file to build Urho3D and this project
 cp -rf web/Urho3DCMakeLists.txt Urho3D-web/Source/CMakeLists.txt
 
 cd Urho3D-web
-# Release mode
+
 ./script/dockerized.sh web
 
 cd ..
